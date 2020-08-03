1168c7841b622f5f7db4d7d0d122df24583af6f0
lammau123 <63532380+lammau123@users.noreply.github.com>
2020-08-03 23:40:02 +0700

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+az_region = "East US"
+az_rg_name = "test-rg"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "az_region" {
+    type = string
+    description = "Azure zones"
+}
+
+variable "az_rg_name" {
+    type = string
+    description = "Azure group name"
+}
